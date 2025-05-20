60c82caad0c31b7e9a092ca0ffa73233f3886462
Sai Rajesh Rapelli <70473193+SaiRajesh06@users.noreply.github.com>
2025-05-20 12:10:21 -0400

    ndex.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-require('dotenv').config(); // ✅ MUST be here
+require('dotenv').config(); 
 
 const app = express();
 app.use(express.json());
